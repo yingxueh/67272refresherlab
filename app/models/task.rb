d20f0ed7c98846cf1338ec7f33a6089a9fe5972d
Yingxue Hu <yingxueh@andrew.cmu.edu>
2021-02-06 23:12:41 -0500

    passed task tests

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
-    #has_many :chores
-    #has_many :children, through: :chores
+    has_many :chores
+    has_many :children, through: :chores
+    validates_presence_of :name
+    validates_numericality_of :points, :greater_than_or_equal_to => 0, :only_integer => true
+
+    scope :alphabetical, -> { order('name') }
+    scope :active, -> { where(active: true) }
 end
