422869ee27dea05d514aaa7b1ed292ded1dd4269
Yingxue Hu <yingxueh@andrew.cmu.edu>
2021-02-06 23:47:05 -0500

    passed chore tests

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,22 @@
 class Chore < ApplicationRecord
     belongs_to :child
     belongs_to :task
+    validates_timeliness_of :due_on
+
+    scope :by_task, -> { joins(:task).order('tasks.name') }
+    scope :chronological, -> { order('due_on', 'completed') }
+    scope :pending, -> { where(completed: false) }
+    scope :done, -> { where(completed: true) }
+    scope :upcoming, -> { where('due_on >= ?', Date.current) } 
+    scope :past, -> { where('due_on < ?', Date.current) }
+    
+    def status
+        if completed==true
+            'Completed'
+        else
+            'Pending'
+        end
+    end
+    
+    
 end
