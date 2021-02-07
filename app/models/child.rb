bffe06197591298da97d66399f78464fb58ab699
Yingxue Hu <yingxueh@andrew.cmu.edu>
2021-02-06 22:41:13 -0500

    starting part 2-model for child

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,6 @@
 class Child < ApplicationRecord
-    
+    has_many :chores
+    has_many :tasks, through: :chores
+    validates :first_name, presence: true
+    validates :last_name, presence: true
 end
