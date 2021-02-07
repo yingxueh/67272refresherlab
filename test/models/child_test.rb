c855c3a5551d97c59c8c6490f7a34462aa2e2dd2
Yingxue Hu <yingxueh@andrew.cmu.edu>
2021-02-06 21:51:24 -0500

    step 5

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -2,9 +2,12 @@ require 'test_helper'
 
 class ChildTest < ActiveSupport::TestCase
   # TODO: Relationship macros
-  
-  # TODO: Validation macros
+  should have_many(:chores)
+  should have_many(:tasks).through(:chores)
 
+  # TODO: Validation macros
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
   # TODO: Context testing
 
 end
